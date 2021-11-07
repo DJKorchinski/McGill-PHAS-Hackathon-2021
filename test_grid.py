14c9dec5d3b4cb60286489be5135a701d711ac7c
Daniel Korchinski <danielkorchinski@gmail.com>
2021-11-06 17:23:20 -0700

    now using numba for gillespie!

--- test_grid.py
+++ test_grid.py
@@ -2,9 +2,13 @@ import gillespie
 import numpy as np 
 import matplotlib.pyplot  as plt 
 
+import time
+
 L = 128
 gstate = gillespie.grid_state(  np.array([1, 0.01,20.]), L , 420)
 # gstate.freeze_site(L//2, L//2)#freeze the central site.
+
+t1 = time.time()
 
 iter = 0
 while(True):
@@ -16,7 +20,9 @@ while(True):
         print('TOO MANY ITERS, DEBUG THIS GARBAGE!')
         break 
 
+t2 = time.time()
+print('took: ',t2-t1)
 print('frozen: ',np.sum(gstate.state == gillespie.FROZEN_STATE),'evaporated: ' ,np.sum(gstate.state == gillespie.EVAPORATED_STATE), 'liquid: ',np.sum(gstate.state == gillespie.LIQUID_STATE))
 
-# plt.imshow(gstate.state)
-# plt.show()
+plt.imshow(gstate.state)
+plt.show()
